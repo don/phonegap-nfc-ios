846c541521eb3bfd6e65c59ae52aee45dec7a785
Don Coleman <dcoleman@chariotsolutions.com>
2018-04-17 18:07:36 -0400

    example code for reading NFC NDEF tags on iOS with phonegap-nfc

---
+++ README.md
@@ -0,0 +1,3 @@
+Sample project for reading NFC tags with NDEF message with Cordova/PhoneGap on iOS.
+
+For more info see https://github.com/chariotsolutions/phonegap-nfc
--- www/js/index.js
+++ www/js/index.js
@@ -28,6 +28,49 @@ var app = {
     // 'pause', 'resume', etc.
     onDeviceReady: function() {
         this.receivedEvent('deviceready');
+
+        // Read NDEF formatted NFC Tags
+        nfc.addNdefListener (
+            function (nfcEvent) {
+                var tag = nfcEvent.tag,
+                    ndefMessage = tag.ndefMessage;
+
+                // Decode payloads for Text and URI records    
+                var decodePayload = function() {
+
+                    var record = ndefMessage[0];
+                    var payloadAsText;
+
+                    if (util.isType(record, ndef.TNF_WELL_KNOWN, ndef.RTD_TEXT)) {
+                        payloadAsText = ndef.textHelper.decodePayload(record.payload);
+                    } else if (util.isType(record, ndef.TNF_WELL_KNOWN, ndef.RTD_URI)) {
+                        payloadAsText = ndef.uriHelper.decodePayload(record.payload);
+                    } else {
+                        console.log('No decoder for TNF ' + ndef.tnfToString(record.tnf));
+                    }
+
+                    if (payloadAsText) {
+                        navigator.notification.alert('', {}, payloadAsText);    
+                    }
+                }
+
+                // dump the raw json of the message
+                // note: real code will need to decode the payload from each record
+                navigator.notification.alert(
+                    JSON.stringify(ndefMessage), 
+                    decodePayload, // decodePayload function is called when OK is clicked
+                    'Raw Tag Data');
+
+            },
+            function () { // success callback
+                console.log("Waiting for NDEF tag");
+                app.addButton();
+            },
+            function (error) { // error callback
+                navigator.notification.alert("Error adding NDEF listener " + JSON.stringify(error));
+            }
+        );
+        
     },
 
     // Update DOM on a Received Event
@@ -40,6 +83,21 @@ var app = {
         receivedElement.setAttribute('style', 'display:block;');
 
         console.log('Received Event: ' + id);
+    },
+
+    addButton: function() {
+        if (cordova.platformId === 'ios') {
+            var div = document.querySelector('#buttonDiv');
+            var button = document.createElement('button');
+            button.innerText = 'Begin NFC Session';
+            button.onclick = function() {
+                nfc.beginSession(
+                    success => console.log('Started session'),
+                    error => navigator.notification.alert('Failed to start session ' + error)
+               );
+            }
+            div.appendChild(button);
+        }
     }
 };
 
