7eb73be719dbc00b807353f9ef4481216a63eddd
Don Coleman <don.coleman@gmail.com>
2020-04-15 21:23:08 -0400

    phonegap-nfc v1.1.0

--- www/js/index.js
+++ www/js/index.js
@@ -1,21 +1,3 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
 var app = {
     // Application Constructor
     initialize: function() {
@@ -23,58 +5,171 @@ var app = {
     },
 
     // deviceready Event Handler
-    //
-    // Bind any cordova events here. Common events are:
-    // 'pause', 'resume', etc.
     onDeviceReady: function() {
-        this.receivedEvent('deviceready');
+        this.receivedDeviceReadyEvent('deviceready');
+        app.addButtons();
 
-        // Read NDEF formatted NFC Tags
+        // NOTE: the NDEF listener only receives data from the deprecated nfc.beginSession 
+        // Not needed for nfc.scanNdef or nfc.scanTag
         nfc.addNdefListener (
             function (nfcEvent) {
-                var tag = nfcEvent.tag,
-                    ndefMessage = tag.ndefMessage;
-
-                // Decode payloads for Text and URI records    
-                var decodePayload = function() {
-
-                    var record = ndefMessage[0];
-                    var payloadAsText;
-
-                    if (util.isType(record, ndef.TNF_WELL_KNOWN, ndef.RTD_TEXT)) {
-                        payloadAsText = ndef.textHelper.decodePayload(record.payload);
-                    } else if (util.isType(record, ndef.TNF_WELL_KNOWN, ndef.RTD_URI)) {
-                        payloadAsText = ndef.uriHelper.decodePayload(record.payload);
-                    } else {
-                        console.log('No decoder for TNF ' + ndef.tnfToString(record.tnf));
-                    }
-
-                    if (payloadAsText) {
-                        navigator.notification.alert('', {}, payloadAsText);    
-                    }
-                }
-
-                // dump the raw json of the message
-                // note: real code will need to decode the payload from each record
-                navigator.notification.alert(
-                    JSON.stringify(ndefMessage), 
-                    decodePayload, // decodePayload function is called when OK is clicked
-                    'Raw Tag Data');
-
+                console.log('NDEF listener received an event');
+                app.showNfcTag(nfcEvent.tag);
             },
             function () { // success callback
-                console.log("Waiting for NDEF tag");
-                app.addButton();
+                console.log("NDEF listener registered.");
             },
             function (error) { // error callback
                 navigator.notification.alert("Error adding NDEF listener " + JSON.stringify(error));
             }
-        );
+        );        
+    },
+
+    showNfcTag: function(tag) {
+
+        pre.innerText = 'NFC Tag'
+        if (tag.id) {
+            pre.innerText += '\n';
+            pre.innerText += '  id: ' + nfc.bytesToHexString(tag.id);
+        }
+        if (tag.type) {
+            pre.innerText += '\n';
+            pre.innerText += '  type: ' + tag.type;
+        }
+        if (tag.isWritable) {
+            pre.innerText += '\n';
+            // TODO convert isWritable to boolean elsewhere
+            pre.innerText += '  isWritable: ' + !!tag.isWritable;
+        }
+        pre.innerText += '\n';
+
+        if (tag.ndefMessage) {
+            tag.ndefMessage.forEach((record, i) => {
+
+                pre.innerText += `\nNDEF Record ${i + 1}\n`;
+    
+                if (util.isType(record, ndef.TNF_WELL_KNOWN, ndef.RTD_TEXT)) {
+                    pre.innerText += '  Text Record\n';
+                    pre.innerText += '    ' + ndef.textHelper.decodePayload(record.payload);
+                } else if (util.isType(record, ndef.TNF_WELL_KNOWN, ndef.RTD_URI)) {
+                    pre.innerText += '  URI Record\n';
+                    pre.innerText += '    ' + ndef.uriHelper.decodePayload(record.payload);
+                } else {
+                    console.log('No decoder for TNF ' + ndef.tnfToString(record.tnf));
+                    pre.innerText += `  TNF ${record.tnf} Record`
+                    pre.innerText += '    ' + JSON.stringify(record);
+                }
+                pre.innerText += '\n';
+            });    
+        }
+
+    },
+
+    clearOutput: function() {
+        pre.innerText = '';
+    },
+
+    showData: function(data) {
+        pre.innerText = data + '\n';
+    },
+
+    showError: function(error) {
+        let message = error;
+        if (error instanceof Error) {
+            message = error.message;
+        }
+        navigator.notification.alert(message);
+    },
+
+    writeNdef: function() {
+
+        app.clearOutput();
         
+        // NDEF message with 2 records
+        var message = [
+            ndef.textRecord("Hello phonegap-nfc!"),
+            ndef.uriRecord("https://chariotsolutions.com")
+        ];
+
+        nfc.write(
+            message,
+            success => app.showData('Message was written to the tag.'),
+            error => app.showError(error)
+       );
+
+    },
+
+    beginSession: function() {
+
+        app.clearOutput();
+
+        // this the old way from iOS 11 use scanNdef instead
+        nfc.beginSession(
+            success => console.log('Started session'),
+            error => navigator.notification.alert(error)
+       );
+
+    },
+
+    startNDEFScan: async function() {
+
+        app.clearOutput();
+
+        // scanNdef replaces beingSession
+        // scanned tag data is returned in the promise
+        try {
+            let tag = await nfc.scanNdef();
+            console.log(JSON.stringify(tag));
+            app.showNfcTag(tag);
+        } catch (err) {
+            app.showError(err);
+        }
+
+    },
+
+    // using the Tag scanner gets the Tag UID but doens't read as many tags
+    startTagScan: async function() {
+
+        app.clearOutput();
+
+        // On iOS 13, we can use the new scanner to include that tag ids
+        try {
+            let tag = await nfc.scanTag();
+            app.showNfcTag(tag);
+        } catch (err) {
+            app.showError(err);
+        }
+    },
+
+    addButtons: function() {
+        if (cordova.platformId === 'ios') {
+            var div = document.querySelector('#buttonDiv');
+
+            // This is the old deprecated way, do not use
+            var button = document.createElement('button');
+            button.innerText = 'Begin NFC Session';
+            button.onclick = app.beginSession;
+            div.appendChild(button);
+
+            button = document.createElement('button');
+            button.innerText = 'Start NDEF Scan';
+            button.onclick = app.startNDEFScan;
+            div.appendChild(button);
+
+            button = document.createElement('button');
+            button.innerText = 'Start Tag Scan';
+            button.onclick = app.startTagScan;
+            div.appendChild(button);
+
+            button = document.createElement('button');
+            button.innerText = 'Write to NFC Tag';
+            button.onclick = app.writeNdef;
+            div.appendChild(button);
+        }
     },
 
     // Update DOM on a Received Event
-    receivedEvent: function(id) {
+    receivedDeviceReadyEvent: function(id) {
         var parentElement = document.getElementById(id);
         var listeningElement = parentElement.querySelector('.listening');
         var receivedElement = parentElement.querySelector('.received');
@@ -83,22 +178,8 @@ var app = {
         receivedElement.setAttribute('style', 'display:block;');
 
         console.log('Received Event: ' + id);
-    },
-
-    addButton: function() {
-        if (cordova.platformId === 'ios') {
-            var div = document.querySelector('#buttonDiv');
-            var button = document.createElement('button');
-            button.innerText = 'Begin NFC Session';
-            button.onclick = function() {
-                nfc.beginSession(
-                    success => console.log('Started session'),
-                    error => navigator.notification.alert('Failed to start session ' + error)
-               );
-            }
-            div.appendChild(button);
-        }
     }
+    
 };
 
 app.initialize();
